811863aa23f7b28270d8823c849404f60bff9ef6
newtonneto <newton.mgs@gmail.com>
2022-03-10 17:12:43 -0300

    custom exceptions

---
+++ src/main/java/com/newton/demo/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.newton.demo.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Problem.Field> fields = new ArrayList<>();
+
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            String name = ((FieldError) error).getField();
+            //String message = error.getDefaultMessage(); //Prove as mensagens sem customizações
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+
+            fields.add(new Problem.Field(name, message));
+        }
+
+        Problem problem = new Problem();
+        problem.setStatus(status.value());
+        problem.setDateTime(LocalDateTime.now());
+        problem.setTitle("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
+        problem.setFields(fields);
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+}
---
+++ src/main/java/com/newton/demo/api/exceptionhandler/Problem.java
@@ -0,0 +1,24 @@
+package com.newton.demo.api.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class Problem {
+    private Integer status;
+    private LocalDateTime dateTime;
+    private String title;
+    private List<Field> fields;
+
+    @AllArgsConstructor
+    @Getter
+    public static class Field {
+        private String name;
+        private String message;
+    }
+}
